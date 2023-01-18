18f264d7ca94f0bf839bd83b5711de79144878fd
NixonThuo <thuonixon808@gmail.com>
2023-01-18 22:00:50 +0300

    CRUD design

--- application/accounts/accounts_controller.py
+++ application/accounts/accounts_controller.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify, render_template
 from application.accounts.accounts_model import Accounts
+from application import db
 
 accounts = Blueprint('accounts', __name__)
 
@@ -7,17 +8,69 @@ accounts = Blueprint('accounts', __name__)
 @accounts.route('/addaccount', methods=['GET', 'POST'])
 def add_account():
     if request.method == "POST":
-        account = request.json
-        print(account)
-        return jsonify({"success": True, "account": account})
+        name = request.form.get("name")
+        address = request.form.get("address")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        status = request.form.get("status")
+        account = Accounts(
+            name=name,
+            address=address,
+            email=email,
+            phone=phone,
+            status=status
+        )
+        db.session.add(account)
+        db.session.commit()
+        return jsonify({"success": True, "accountid": account.accountid})
     else:
         return render_template("accounts/index.html")
 
 
-@accounts.route('/getaccount/<int:accountid>', methods=['GET', 'POST'])
+@accounts.route('/listaccounts', methods=['GET'])
+def listaccounts():
+    accounts = Accounts.query.all()
+    return render_template("accounts/accounts_list.html", accounts=accounts)
+
+
+@accounts.route('/updateaccount', methods=['GET', 'POST'])
+def update_account():
+    if request.method == "POST":
+        accountid = request.form.get("accountid")
+        name = request.form.get("name")
+        address = request.form.get("address")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        status = request.form.get("status")
+        account = Accounts.query.filter_by(
+            accountid=accountid
+        ).first()
+        account.name = name
+        account.address = address
+        account.email = email
+        account.phone = phone
+        account.status = status
+        db.session.commit()
+        return jsonify({"success": True, "accountid": account.accountid, "message": "account updated"})
+    else:
+        return render_template("accounts/index.html")
+
+
+@accounts.route('/getaccount/<int:accountid>', methods=['GET'])
 def get_account(accountid):
     """fetch account details"""
-    acc = Accounts.query.filter_by(
+    account = Accounts.query.filter_by(
         accountid=accountid
     ).first()
-    return jsonify({"account": acc})
+    if account:
+        return render_template("accounts/account_update.html", account=account)
+    return jsonify({"error": "account not found"})
+
+
+@accounts.route('/deleteaccount/<int:accountid>', methods=['GET'])
+def delete_account(accountid):
+    account = Accounts.query.filter_by(
+        accountid=accountid
+    ).first()
+    db.session.delete(account)
+    return "Deleted"
--- application/policy/policy_controller.py
+++ application/policy/policy_controller.py
@@ -1,36 +1,74 @@
 """policy blueprint"""
 from flask import Blueprint, request, jsonify, render_template
+from application.policy.policy_model import Policies
+from application.accounts.accounts_model import Accounts
+from application import db
 
 policy = Blueprint('policy', __name__)
 
 
 @policy.route('/addpolicy', methods=['GET', 'POST'])
 def add_policy():
+    accounts = Accounts.query.all()
     if request.method == "POST":
-        policy = request.json
-        print(policy)
-        return jsonify({"success": True, "policy": policy})
+        accountid = request.form.get("accountid")
+        policytype = request.form.get("policytype")
+        policy = Policies(
+            accountid=accountid,
+            policytype=policytype
+        )
+        db.session.add(policy)
+        db.session.commit()
+        return jsonify({"success": True, "policyid": policy.policyid})
+    else:
+        return render_template("policy/index.html", accounts=accounts)
+
+
+@policy.route('/listpolicies', methods=['GET'])
+def listpolicies():
+    policies = Policies.query.all()
+    return render_template("policy/policy_list.html", policies=policies)
+
+
+@policy.route('/updatepolicy', methods=['GET', 'POST'])
+def update_policy():
+    if request.method == "POST":
+        accountid = request.form.get("accountid")
+        policyid = request.form.get("policyid")
+        policytype = request.form.get("policytype")
+        policy = Policies.query.filter_by(
+            policyid=policyid,
+        ).first()
+        policy.accountid = accountid
+        policy.policyid = policyid
+        policy.policytype = policytype
+        db.session.commit()
+        return jsonify({"success": True, "policyid": policy.policyid, "message": "policy updated"})
     else:
         return render_template("policy/index.html")
 
 
-@policy.route('/getpolicy/<int:policyid>', methods=['POST'])
+@policy.route('/getpolicy/<int:policyid>', methods=['GET'])
 def get_policy(policyid):
     """policy chat page"""
-    return "True"
-
-
-@policy.route('/deletepolicy/<int:policyid>', methods=['DELETE'])
-def delete_policy(policyid):
-    """policy chat page"""
-    return "True"
-
-
-@policy.route('/updatepolicy', methods=['POST'])
-def update_policy(policyid):
-    if request.method == "POST":
-        policy = request.json
-        print(policy)
-        return jsonify({"success": True, "policy": policy})
+    policy = Policies.query.filter_by(
+        policyid=policyid
+    ).first()
+    selectedaccount = Accounts.query.filter_by(
+        accountid=policy.accountid
+    ).first()
+    accounts = Accounts.query.all()
+    if policy:
+        return render_template("policy/policy_update.html", policy=policy,
+                               selectedaccount=selectedaccount, accounts=accounts)
     else:
-        return jsonify({"message": "should be POST request"})
+        return jsonify({"error": "policy not found"})
+
+
+@policy.route('/deletepolicy/<int:policyid>', methods=['GET'])
+def delete_policy(policyid):
+    policy = Policies.query.filter_by(
+        policyid=policyid
+    ).first()
+    db.session.delete(policy)
+    return "Deleted"
